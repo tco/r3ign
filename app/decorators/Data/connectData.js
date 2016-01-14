7c8d5ecaf901fddd67484cb68433b1e729fdd9ce
Antti Rintala <antti.rintala@gmail.com>
2016-01-14 08:06:54 +0200

    Added connectData decorator

---
+++ app/decorators/Data/connectData.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import hoistStatics         from 'hoist-non-react-statics';
+
+/*
+ Note:
+ When this decorator is used, it MUST be the first (outermost) decorator.
+ Otherwise, we cannot find and call the fetchData methods.
+ */
+
+export default function connectData(fetchData, clientOnly = false) {
+
+    return function wrapWithFetchData(WrappedComponent) {
+        class ConnectData extends Component {
+            render() {
+                return <WrappedComponent { ...this.props } />;
+            }
+        }
+
+        ConnectData.fetchData = fetchData;
+        ConnectData.fetchInClientOnly = clientOnly;
+
+        return hoistStatics(ConnectData, WrappedComponent);
+    };
+}
