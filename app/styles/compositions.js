cd50bccdd016ae1d434060ac4a0cfdef277b8ed0
Antti Rintala <antti.rintala@gmail.com>
2016-02-24 09:07:34 +0200

    Added sample compositions and definitions

---
+++ app/styles/compositions.js
@@ -0,0 +1,7 @@
+import { display, colors } from './definitions.js';
+
+export const blackContainer = {
+    display: display.block,
+    backgroundColor: colors.black,
+    color: colors.white
+};
---
+++ app/styles/definitions.js
@@ -0,0 +1,10 @@
+export const colors = {
+    black: '#000000',
+    white: '#ffffff'
+};
+
+export const display = {
+    block:          'block',
+    inlineBlock:    'inline-block',
+    none:           'none'
+};
